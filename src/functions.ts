31bca0e3cc671029ca73af53666bc4fa39915768
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-04 00:12:20 +0800

    Setup npm package

---
+++ src/functions.ts
@@ -0,0 +1,5 @@
+import { eventListenerProps } from './types'
+
+export function eventListeners({var1, var2, var3}: eventListenerProps) {
+  console.log(var1)
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type eventListenerProps = {
+  var1: string;
+  var2?: string;
+  var3?: number;
+};
