819e6b14b0951fe2b92d061e8306bfcaccb073c9
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-07 07:38:59 +0800

    Add types to the methods and properties. Export the enire class to be used

--- src/functions.ts
+++ src/functions.ts
@@ -1,101 +1,83 @@
-// import { eventListenerProps } from './types'
-
-// export function eventListeners({var1, var2, var3}: eventListenerProps) {
-//   console.log(var1)
-// }
-
-class HTML_Class_Modifier {
+export default class HTML_Class_Modifier {
   constructor() {
-    this.setPointerEvents()
-    this.eventListeners()
+    this.setPointerEvents();
+    this.eventListeners();
   }
 
-  modifiers = {
-    "toggleFrom": function(element) {
-      this.modifyElements(element.dataset.toggleFrom, element.dataset.toggleClass, 'toggle')
+  modifiers: {
+    [key: string]: (element: HTMLElement) => void;
+  } = {
+    toggleFrom: (element: HTMLElement) => {
+      this.modifyElements(element.dataset.toggleFrom!, element.dataset.toggleClass!, 'toggle');
     },
-    "removeFrom": function(element) {
-      this.modifyElements(element.dataset.removeFrom, element.dataset.removeClass, 'remove')
+    removeFrom: (element: HTMLElement) => {
+      this.modifyElements(element.dataset.removeFrom!, element.dataset.removeClass!, 'remove');
     },
-    "addTo": function(element) {
-      this.modifyElements(element.dataset.addTo, element.dataset.addClass, 'add')
-    }
+    addTo: (element: HTMLElement) => {
+      this.modifyElements(element.dataset.addTo!, element.dataset.addClass!, 'add');
+    },
+  };
+
+  formatAttributeValues(data: string): string[][] {
+    return data.split('|').map(selector => {
+      return selector.split(',').map(element => element.trim());
+    });
   }
 
-  formatAttributeValues = function(data)
-  {
-    let result = data.split('|').map(selector => {
-      return selector.split(',').map(element => element.trim())
-    })
-    return result
-  }
-
-  modifyElements = function(selectors, classes, action)
-  {
+  modifyElements(selectors: string, classes: string, action: 'toggle' | 'add' | 'remove'): void {
     if (!selectors || !classes) {
-      alert('Please check the required attributes and make sure they are not empty. \n\n For more details, check the documentation at https://github.com/martincipriano/html-class-modifier')
-      return
+      alert('Please check the required attributes and make sure they are not empty. \n\n For more details, check the documentation at https://github.com/martincipriano/html-class-modifier');
+      return;
     }
 
-    selectors = this.formatAttributeValues(selectors)
-    classes = this.formatAttributeValues(classes)
+    const formattedSelectors = this.formatAttributeValues(selectors);
+    const formattedClasses = this.formatAttributeValues(classes);
 
-    // Value might be an ID or a class
-    // Loop through the array of selectors to accommodate classes
-    selectors.forEach(function(selector, key) {
-      let targetElements = document.querySelectorAll(selector)
+    formattedSelectors.forEach((selectorArray, key) => {
+      const targetElements = document.querySelectorAll(selectorArray.join(','));
 
-      // Check if the target element exists
       if (targetElements.length) {
-
-        // Loop through the target elements
-        // This will handle multiple target elements having the same class
-        targetElements.forEach(function(targetElement) {
-          classes[key].forEach(className => {
+        targetElements.forEach(targetElement => {
+          formattedClasses[key].forEach(className => {
             switch (action) {
               case 'toggle':
-                targetElement.classList.toggle(className)
-                break
+                targetElement.classList.toggle(className);
+                break;
               case 'remove':
-                targetElement.classList.remove(className)
-                break
+                targetElement.classList.remove(className);
+                break;
               case 'add':
-                targetElement.classList.add(className)
-                break
+                targetElement.classList.add(className);
+                break;
             }
-          })
-        })
+          });
+        });
       }
-    })
+    });
   }
 
-  setPointerEvents = function()
-  {
-    let style = document.createElement('style')
-    style.textContent = `[data-toggle-from] *, [data-add-to] *, [data-remove-from] * { pointer-events: none }`
-    document.head.appendChild(style)
+  setPointerEvents(): void {
+    const style = document.createElement('style');
+    style.textContent = `
+      [data-toggle-from] *, 
+      [data-add-to] *, 
+      [data-remove-from] * { 
+        pointer-events: none; 
+      }`;
+    document.head.appendChild(style);
   }
 
-  eventListeners = function()
-  {
+  eventListeners(): void {
+    document.body.addEventListener('click', (event: MouseEvent) => {
+      const element = event.target as HTMLElement;
 
-    // Delegate the click event to the body element for elements added dynamically
-    document.body.addEventListener('click', function(event) {
-      let element = event.target
-
-      // Check if the element has the appropriate data attributes for running a class modifier sequence
       if (element.hasAttribute('data-toggle-from') || element.hasAttribute('data-add-to') || element.hasAttribute('data-remove-from')) {
-
-        // Instead of adding an event listener for click,
-        // loop through the dataset (DOMStringMap) and execute corresponding functions in orde
         for (const key in element.dataset) {
-
-          // Run the corresponding modifier function if it exists
-          if (this.modifiers[key]) {
-            this.modifiers[key](element)
+          if (Object.prototype.hasOwnProperty.call(element.dataset, key) && this.modifiers[key]) {
+            this.modifiers[key](element);
           }
         }
       }
-    })
+    });
   }
 }
