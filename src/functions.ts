f780ed79e4c2a4e007fb4608a15f71cb6f0de56d
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-07 07:18:01 +0800

    Create a method that will parse the data-attribute's contents and return them as an array. Apply this formatter to the selectors and classes passed in the element modifier method

--- src/functions.ts
+++ src/functions.ts
@@ -18,17 +18,28 @@ class HTML_Class_Modifier {
     }
   }
 
+  formatAttributeValues = function(data)
+  {
+    let result = data.split('|').map(selector => {
+      return selector.split(',').map(element => element.trim())
+    })
+    return result
+  }
+
   modifyElements = function(selectors, classes, action) {
     if (!selectors || !classes) {
       alert('Please check the required attributes and make sure they are not empty. \n\n For more details, check the documentation at https://github.com/martincipriano/html-class-modifier')
       return
     }
+
+    selectors = this.formatAttributeValues(selectors)
+    classes = this.formatAttributeValues(classes)
   }
 
   setPointerEvents = function()
   {
     let style = document.createElement('style')
-    style.textContent = `[data-toggle-from] *, [data-add-to] *, [data-remove-from] * { pointer-events: none; }`
+    style.textContent = `[data-toggle-from] *, [data-add-to] *, [data-remove-from] * { pointer-events: none }`
     document.head.appendChild(style)
   }
 }
