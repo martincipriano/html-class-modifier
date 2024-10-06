576cde3ace55ca3508e1adaa3c7e3789c25b05e0
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-07 07:26:55 +0800

    Delegate click event to accommodate elements added in the DOM dynamically. Instead of running the modifiers directly, loop through the dataset and execute them in order if a corresponding method exists.

--- src/functions.ts
+++ src/functions.ts
@@ -74,4 +74,27 @@ class HTML_Class_Modifier {
     style.textContent = `[data-toggle-from] *, [data-add-to] *, [data-remove-from] * { pointer-events: none }`
     document.head.appendChild(style)
   }
+
+  eventListeners = function()
+  {
+
+    // Delegate the click event to the body element for elements added dynamically
+    document.body.addEventListener('click', function(event) {
+      let element = event.target
+
+      // Check if the element has the appropriate data attributes for running a class modifier sequence
+      if (element.hasAttribute('data-toggle-from') || element.hasAttribute('data-add-to') || element.hasAttribute('data-remove-from')) {
+
+        // Instead of adding an event listener for click,
+        // loop through the dataset (DOMStringMap) and execute corresponding functions in orde
+        for (const key in element.dataset) {
+
+          // Run the corresponding modifier function if it exists
+          if (this.modifiers[key]) {
+            this.modifiers[key](element)
+          }
+        }
+      }
+    })
+  }
 }
