720244df24d75230f88dab257d9c55cc4b287a98
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-07 07:00:01 +0800

    Add a constructor and css that will prevent the trigger-element's children from being clicked fixing the issue with  JS event.target.

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,18 @@
-import { eventListenerProps } from './types'
+// import { eventListenerProps } from './types'
 
-export function eventListeners({var1, var2, var3}: eventListenerProps) {
-  console.log(var1)
+// export function eventListeners({var1, var2, var3}: eventListenerProps) {
+//   console.log(var1)
+// }
+
+class HTML_Class_Modifier {
+  constructor() {
+    this.setPointerEvents()
+  }
+
+  setPointerEvents = function()
+  {
+    let style = document.createElement('style')
+    style.textContent = `[data-toggle-from] *, [data-add-to] *, [data-remove-from] * { pointer-events: none; }`
+    document.head.appendChild(style)
+  }
 }
