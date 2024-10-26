e30d4100ac90c2cb80353bcea621bdd9cc9c44a0
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-26 22:18:26 +0800

    Minify the output

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   format: ['cjs', 'esm'],
-  entry: ['./src/index.ts'],
+  entry: ['./src/html-class-modifier.ts'],
   dts: true,
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
+  minify: true,
+  outDir: 'dist',
+  outExtension: ({ format }) => ({
+    js: format === 'cjs' ? '.js' : '.mjs'
+  }),
 });
